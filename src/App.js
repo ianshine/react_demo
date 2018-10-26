319adc51ea1e99ddfc3f058894cd81bf944c52f7
ian <ianshine@hotmail.com>
2018-10-26 16:27:23 +0800

    react 表单学习

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Please write an essay about your favorite DOM element.',
+      name: '',
+      select: 'coconut',
+      isTrue: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // handleChange(event) {
+  //   this.setState({value: event.target.name + event.target.text});
+  // }
+
+  // handleChange(event){
+  //   let o = {};
+  //   o[event.target.name] = event.target.value;
+  //   this.setState(o);
+  //   }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.name + this.state.text + this.state.select);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h2>查询用户是否存在功能</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" name = "name" value={this.state.name} onChange={this.handleChange} />
+            <br />
+
+            text:
+            <textarea type="text" name = "text" value={this.state.text} onChange={this.handleChange} />
+            <br />
+
+            Pick your favorite La Croix flavor:
+            <select name = "select" value={this.state.select} onChange={this.handleChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+            <br />
+
+            Is true:
+            <input
+              name="isTrue"
+              type="checkbox"
+              checked={this.state.isTrue}
+              onChange={this.handleChange} />
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default App;
