164ce3aa970a439a35fccd5a844377aee1901834
ianshine <ianshine@hotmail.com>
2018-11-04 22:06:05 +0800

    ant-desgin代码更新

--- src/AntDesgin/TodoList.js
+++ src/AntDesgin/TodoList.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
-import { Input } from 'antd';
-// import 'antd/dist/antd.css';
+import { Input,Button,List } from 'antd';
+import 'antd/dist/antd.css';
+
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ];
 
 class TodoList extends React.Component {
 
     render() {
         return (
-                    <div>
-                        <Input placeholder='todo info'/>
-                    </div>
+            <div>
+                {/* 边框上方，左侧 边距 */}
+                <div style={{marginTop:'10px', marginLeft:'10px'}}>
+                    {/* input宽度，右侧 边距 */}
+                    <Input placeholder='todo info' style={{width:'300px', marginRight:'10px'}}/>
+                    <Button type="primary">提交</Button>
+                </div>
+                
+                {/* marginTop边框上方，左侧 边距 ，bordered列表是否需要边框 */}
+                <div> 
+                    <List
+                        style={{marginTop:'50px', marginLeft:'10px', width:'360px'}}
+                        bordered 
+                        dataSource={data}
+                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                    />
+                </div>
+            </div>
             )
     }
 } 
